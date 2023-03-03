a2073d43d16323f19a089e05243b596494aa1519
Aryan Sethi <46134586+neohacker18@users.noreply.github.com>
2023-03-03 20:12:24 +0530

    Added endpoint for Bar chart

--- api/controllers/salesController.js
+++ api/controllers/salesController.js
@@ -14,25 +14,104 @@ function getMonth(month) {
   else if (month === "october") return "10";
   else if (month === "november") return "11";
   else if (month === "december") return "12";
-  return month
+  return month;
 }
 
 exports.getSalesData = (req, res, next) => {
   const month = req.params.month;
   SalesData.find({ monthOfSale: getMonth(month) })
     .then((data) => {
-        let totalSaleAmount=0,totalNumberOfSoldItems=0,totalNumberOfNotSoldItems=0;
-        for(let x=0;x<data.length;x++){
-            if(data[x].sold){
-                totalNumberOfSoldItems+=1;
-                totalSaleAmount+=data[x].price;
-            }
-            else totalNumberOfNotSoldItems+=1;
-        }
+      let totalSaleAmount = 0,
+        totalNumberOfSoldItems = 0,
+        totalNumberOfNotSoldItems = 0;
+      for (let x = 0; x < data.length; x++) {
+        if (data[x].sold) {
+          totalNumberOfSoldItems += 1;
+          totalSaleAmount += data[x].price;
+        } else totalNumberOfNotSoldItems += 1;
+      }
       res.status(200).json({
         totalSaleAmount: totalSaleAmount,
-        totalNumberOfSoldItems:totalNumberOfSoldItems,
-        totalNumberOfNotSoldItems:totalNumberOfNotSoldItems,
+        totalNumberOfSoldItems: totalNumberOfSoldItems,
+        totalNumberOfNotSoldItems: totalNumberOfNotSoldItems,
+      });
+    })
+    .catch((err) => {
+      console.log(err);
+    });
+};
+
+exports.getBarChartData = (req, res, next) => {
+  const month = req.params.month;
+  SalesData.find({ monthOfSale: getMonth(month) })
+    .then((data) => {
+      let itemsInPriceRange = {
+        "0-100": 0,
+        "101-200": 0,
+        "201-300": 0,
+        "301-400": 0,
+        "401-500": 0,
+        "501-600": 0,
+        "601-700": 0,
+        "701-800": 0,
+        "801-900": 0,
+        "901-above": 0,
+      };
+
+      for (let x = 0; x < data.length; x++) {
+        const price = data[x].price;
+        if(0<=price && price<=100) itemsInPriceRange['0-100']++;
+        else if(101<=price && price<=200) itemsInPriceRange['101-200']++;
+        else if(201<=price && price<=300) itemsInPriceRange['201-300']++;
+        else if(301<=price && price<=400) itemsInPriceRange['301-400']++;
+        else if(401<=price && price<=500) itemsInPriceRange['401-500']++;
+        else if(501<=price && price<=600) itemsInPriceRange['501-600']++;
+        else if(601<=price && price<=700) itemsInPriceRange['601-700']++;
+        else if(701<=price && price<=800) itemsInPriceRange['701-800']++;
+        else if(801<=price && price<=900) itemsInPriceRange['801-900']++;
+        else itemsInPriceRange['901-above']++;
+      }
+      res.status(200).json({
+        itemsInPriceRange: itemsInPriceRange,
+      });
+    })
+    .catch((err) => {
+      console.log(err);
+    });
+};
+
+exports.getPieChartData = (req, res, next) => {
+  const month = req.params.month;
+  SalesData.find({ monthOfSale: getMonth(month) })
+    .then((data) => {
+      let itemsInPriceRange = {
+        "0-100": 0,
+        "101-200": 0,
+        "201-300": 0,
+        "301-400": 0,
+        "401-500": 0,
+        "501-600": 0,
+        "601-700": 0,
+        "701-800": 0,
+        "801-900": 0,
+        "901-above": 0,
+      };
+
+      for (let x = 0; x < data.length; x++) {
+        const price = data[x].price;
+        if(0<=price && price<=100) itemsInPriceRange['0-100']++;
+        else if(101<=price && price<=200) itemsInPriceRange['101-200']++;
+        else if(201<=price && price<=300) itemsInPriceRange['201-300']++;
+        else if(301<=price && price<=400) itemsInPriceRange['301-400']++;
+        else if(401<=price && price<=500) itemsInPriceRange['401-500']++;
+        else if(501<=price && price<=600) itemsInPriceRange['501-600']++;
+        else if(601<=price && price<=700) itemsInPriceRange['601-700']++;
+        else if(701<=price && price<=800) itemsInPriceRange['701-800']++;
+        else if(801<=price && price<=900) itemsInPriceRange['801-900']++;
+        else itemsInPriceRange['901-above']++;
+      }
+      res.status(200).json({
+        itemsInPriceRange: itemsInPriceRange,
       });
     })
     .catch((err) => {
--- api/routes/sales.js
+++ api/routes/sales.js
@@ -3,5 +3,7 @@ const router = express.Router();
 const salesController = require("../controllers/salesController");
 
 router.get("/getSalesData/:month", salesController.getSalesData);
+router.get("/getBarChartData/:month", salesController.getBarChartData);
+router.get("/getPieChartData/:month", salesController.getPieChartData);
 
 module.exports = router;
